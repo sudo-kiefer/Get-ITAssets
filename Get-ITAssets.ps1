e88a75b4d382e38eea2942c197258968f6f75926
Kiefer Gambelin <73907782+sudo-kiefer@users.noreply.github.com>
2024-03-04 12:08:08 -0800

    Add Get-ITAssets.ps1

---
+++ Get-ITAssets.ps1
@@ -0,0 +1,140 @@
+[CmdletBinding()]
+  PARAM (
+    [Parameter(ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$True)]
+    [String[]]$ComputerName = $env:ComputerName
+  )
+
+$serialnumber = Get-WmiObject win32_bios -ComputerName $ComputerName | select -ExpandProperty serialnumber
+
+$username = Get-WmiObject win32_computersystem -ComputerName $ComputerName | select -ExpandProperty username
+
+$modelnumber = Get-WmiObject win32_computersystem -ComputerName $ComputerName | Select -ExpandProperty Model
+
+$manufacturer = Get-WmiObject win32_computersystem -ComputerName $ComputerName | Select -ExpandProperty Manufacturer
+
+$ManufacturerHash = @{ 
+    "AAC" =	"AcerView";
+    "ACR" = "Acer";
+    "AOC" = "AOC";
+    "AIC" = "AG Neovo";
+    "APP" = "Apple Computer";
+    "AST" = "AST Research";
+    "AUO" = "Asus";
+    "BNQ" = "BenQ";
+    "CMO" = "Acer";
+    "CPL" = "Compal";
+    "CPQ" = "Compaq";
+    "CPT" = "Chunghwa Pciture Tubes, Ltd.";
+    "CTX" = "CTX";
+    "DEC" = "DEC";
+    "DEL" = "Dell";
+    "DPC" = "Delta";
+    "DWE" = "Daewoo";
+    "EIZ" = "EIZO";
+    "ELS" = "ELSA";
+    "ENC" = "EIZO";
+    "EPI" = "Envision";
+    "FCM" = "Funai";
+    "FUJ" = "Fujitsu";
+    "FUS" = "Fujitsu-Siemens";
+    "GSM" = "LG Electronics";
+    "GWY" = "Gateway 2000";
+    "HEI" = "Hyundai";
+    "HIT" = "Hyundai";
+    "HSL" = "Hansol";
+    "HTC" = "Hitachi/Nissei";
+    "HWP" = "HP";
+    "IBM" = "IBM";
+    "ICL" = "Fujitsu ICL";
+    "IVM" = "Iiyama";
+    "KDS" = "Korea Data Systems";
+    "LEN" = "Lenovo";
+    "LGD" = "Asus";
+    "LPL" = "Fujitsu";
+    "MAX" = "Belinea"; 
+    "MEI" = "Panasonic";
+    "MEL" = "Mitsubishi Electronics";
+    "MS_" = "Panasonic";
+    "NAN" = "Nanao";
+    "NEC" = "NEC";
+    "NOK" = "Nokia Data";
+    "NVD" = "Fujitsu";
+    "OPT" = "Optoma";
+    "PHL" = "Philips";
+    "REL" = "Relisys";
+    "SAN" = "Samsung";
+    "SAM" = "Samsung";
+    "SBI" = "Smarttech";
+    "SGI" = "SGI";
+    "SNY" = "Sony";
+    "SRC" = "Shamrock";
+    "SUN" = "Sun Microsystems";
+    "SEC" = "Hewlett-Packard";
+    "TAT" = "Tatung";
+    "TOS" = "Toshiba";
+    "TSB" = "Toshiba";
+    "VSC" = "ViewSonic";
+    "ZCM" = "Zenith";
+    "UNK" = "Unknown";
+    "_YV" = "Fujitsu";
+      }
+      
+  
+  #Takes each computer specified and runs the following code:
+  ForEach ($Computer in $ComputerName) {
+  
+    #Grabs the Monitor objects from WMI
+    $Monitors = Get-WmiObject -Namespace "root\WMI" -Class "WMIMonitorID" -ComputerName $Computer -ErrorAction SilentlyContinue
+    
+    #Creates an empty array to hold the data
+    $Monitor_Array = @()
+    
+    
+    #Takes each monitor object found and runs the following code:
+    ForEach ($Monitor in $Monitors) {
+      
+      #Grabs respective data and converts it from ASCII encoding and removes any trailing ASCII null values
+      If ([System.Text.Encoding]::ASCII.GetString($Monitor.UserFriendlyName) -ne $null) {
+        $Mon_Model = ([System.Text.Encoding]::ASCII.GetString($Monitor.UserFriendlyName)).Replace("$([char]0x0000)","")
+      } else {
+        $Mon_Model = $null
+      }
+      $Mon_Serial_Number = ([System.Text.Encoding]::ASCII.GetString($Monitor.SerialNumberID)).Replace("$([char]0x0000)","")
+      $Mon_Attached_Computer = ($Monitor.PSComputerName).Replace("$([char]0x0000)","")
+      $Mon_Manufacturer = ([System.Text.Encoding]::ASCII.GetString($Monitor.ManufacturerName)).Replace("$([char]0x0000)","")
+      
+      #Filters out "non monitors". Place any of your own filters here. These two are all-in-one computers with built in displays. I don't need the info from these.
+      If ($Mon_Model -like "*800 AIO*" -or $Mon_Model -like "*8300 AiO*") {Break}
+      
+      #Sets a friendly name based on the hash table above. If no entry found sets it to the original 3 character code
+      $Mon_Manufacturer_Friendly = $ManufacturerHash.$Mon_Manufacturer
+      If ($Mon_Manufacturer_Friendly -eq $null) {
+        $Mon_Manufacturer_Friendly = $Mon_Manufacturer
+      }
+      
+      #Creates a custom monitor object and fills it with 4 NoteProperty members and the respective data
+      $Monitor_Obj = [PSCustomObject]@{
+        Manufacturer     = $Mon_Manufacturer_Friendly
+        Model            = $Mon_Model
+        SerialNumber     = $Mon_Serial_Number
+        AttachedComputer = $Mon_Attached_Computer
+      }
+      
+      #Appends the object to the array
+      $Monitor_Array += $Monitor_Obj
+
+    } #End ForEach Monitor
+    
+}
+
+
+Write-Host " "
+Write-Host "$ComputerName info:"
+Write-Host " "
+Write-Host "User: $username"
+Write-Host "Manufacturer: $manufacturer"
+Write-Host "Model Number: $modelnumber"
+Write-Host "Serial Number: $serialnumber"
+Write-Host " "
+Write-Host "Monitor(s) info:"
+echo $Monitor_Array
